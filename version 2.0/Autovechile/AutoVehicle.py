a695ecde642607f3cf92e4cc1da47aaf4a620407
MG-98 <s-mahmoud_gamal@zewailcity.edu.eg>
2021-06-01 00:56:19 +0200

    Traffic light agent

---
+++ version 2.0/Autovechile/AutoVehicle.py
@@ -0,0 +1,218 @@
+import sys , os
+sys.path.append(os.path.join(os.environ.get("SUMO_HOME"), 'tools'))
+import numpy as np
+import traci
+import math
+from random import randrange
+
+class AutoVehicle:
+    '''
+    This is a class for the main agent in our system - the autonomous vehicle.
+    The class mainly used for getting the state of the vehicle and applying actions.
+    '''
+
+    def __init__(self, ID):
+        ################# Parameters ###################
+        self.dT = 0.5 # This is the step value
+        self.accl = 1   # We use cell/step^2 convention
+        self.currentspeed = 0  # We use cell/step convention
+        self._pos = (0, 0)
+        self._step = -1
+        self.TopLeft = (143.6, 156.40)
+        self.BotRight = (156.40 , 143.6)
+        self.intersection_Xlen = self.BotRight[0] - self.TopLeft[0]
+        self.intersection_Ylen = self.BotRight[1] - self.TopLeft[1]
+        self.cells_per_side = 24
+        self.cell_len = self.intersection_Ylen/self.cells_per_side
+        self.dT = .5 #sec
+        self.V_range = list(range(5))
+        self.A_range = [-1,0,1]
+        self.queuelen = 0
+        ################## Dicts  ######################
+        self.speedDict = {0:0, 1: 1.75 , 2: 3.5 , 3: 5.25 , 4: 7} # Mapping from cell/step to m/s
+        self.maxspeed = 7 # We use m/s convention
+        self.accSumo = 3.5 # m/s^2
+        # self.set
+        ################# Intersection dimensions ################
+        self.TopLeft = (143.6, 156.40)
+        self.BotRight = (156.40, 143.6)
+        ################################################
+
+        self.isPrevIndividual = True
+        self._hot_time = -1
+        self.ID = ID
+        traci.vehicle.setMaxSpeed(self.ID ,self.maxspeed )
+        traci.vehicle.setAccel(self.ID , self.accl)
+        self.L, self.W = traci.vehicle.getLength(self.ID), traci.vehicle.getWidth(self.ID)
+        self.DoI = 0
+
+
+
+
+    ###################################################################
+    def inIntersection(self):
+        self.pos = traci.vehicle.getPosition(self.ID) #(x,y)
+        print(self.pos)
+        self.inter = False
+        if self.pos[0] > self.TopLeft[0] and  self.pos[0] < self.BotRight[0] and self.pos[1] < self.TopLeft[1] and  self.pos[1] > self.BotRight[1]:
+            self.inter = True
+        if not self.inter:
+            self.lane = traci.vehicle.getLaneID(self.ID)
+            self.queuelen = traci.lane.getLastStepVehicleNumber(self.lane)
+        print(self.inter)
+        return self.inter
+
+    ############################# Getter ###############################
+
+    def getCells(self,lane_len,side_cells,intersection_width): #retracted
+        x,y = traci.vehicle.getPosition(self.ID)
+        x,y = x-lane_len,y-lane_len
+        x_i,y_i = x//(intersection_width/side_cells),y//(intersection_width/side_cells)
+        x_i,y_i = int(x_i),int(y_i)
+        if intersection_width>=x>=0 and intersection_width>=y>=0:
+            print(x_i,y_i)
+        return x_i,y_i
+
+    def m_sec_2_cell_step(self,v):
+        m_step = v*self.dT 
+        cell_step = m_step / self.cell_len
+        if cell_step > self.V_range[-1]:
+            print("Warning, over max speed.")
+            cell_step = self.V_range[-1]
+        else:
+            cell_step = round(cell_step)
+        return cell_step
+
+    def getPose(self):
+        lane_pose = traci.vehicle.getLanePosition(self.ID)
+        print(lane_pose)
+        position=traci.vehicle.getPosition(self.ID)
+        print(position)
+        pos = traci.vehicle.getLateralLanePosition(self.ID)
+        print(pos)
+
+        #self.pose = lane_pose + self.route * 500
+
+    def getBoxintersection(self):
+        lane_pose = traci.vehicle.getLanePosition(self.ID)
+        edge_id  = traci.vehicle.getRoadID(self.ID)
+        position = traci.vehicle.getPosition(self.ID)
+        print(traci.lane.getWidth(traci.vehicle.getLaneID(self.ID)))
+        #print(position)
+        #and edge_id in [":0_9" , ":0_1" , ":0_5" ,":0_13" , ":0_8" , ":0_0" , ":0_12" , ":0_4"]
+        if lane_pose >0 and lane_pose<20 and position[0]>500 and edge_id in [":0_9" , ":0_1" , ":0_5" ,":0_13" , ":0_8" , ":0_0" , ":0_12" , ":0_4"]:
+            print(lane_pose )
+            print("I am in intersection" )
+            print("I am in position" , round(lane_pose/5 ),"in edge" , edge_id)
+
+
+    def UpdateStatus(self):
+        self.spd = traci.vehicle.getSpeed(self.ID)
+        self.accel = traci.vehicle.getAcceleration(self.ID)
+        self.lane = traci.vehicle.getLaneID(self.ID)
+        self.edge = traci.vehicle.getRoadID(self.ID)
+        self.angle = traci.vehicle.getAngle(self.ID)
+        # self.old_pos = self.pos
+        self.pos = traci.vehicle.getPosition(self.ID) #(x,y)
+        self.currentspeed = self.m_sec_2_cell_step(self.spd)
+        self.get_current_cells()
+        #bad hot fix, separate pos variable
+        # traci.vehicle.setSpeed(self.ID, self.speedDict[self.currentspeed])
+
+    def hot_update_pos(self):
+        if self._hot_time == traci.simulation.getTime():
+            return
+        self._step = traci.simulation.getTime()
+        self._pos_old = self._pos
+        self._pos = traci.vehicle.getPosition(self.ID)
+
+    def get_time_step_distance(self):
+
+        self.hot_update_pos()
+        distance = np.sqrt((self._pos[0] - self._pos_old[0])**2+(self._pos[1] - self._pos_old[1])**2)
+        return distance
+
+    def point_to_cell(self,point):
+        x,y = point[0],point[1]
+        x,y = x-self.TopLeft[0],y-self.TopLeft[1]
+        x_i,y_i = x//(self.cell_len),y//(self.cell_len)
+        return x_i,y_i
+
+    def get_current_cells(self):
+        angle_d = (-self.angle + 90)% 360
+        angle = math.radians(angle_d)
+        #print(angle_d)
+        pos = self.pos #position at center of front dumper
+        l, w = self.L, self.W
+
+        front_left = (pos[0] + w*.5*math.cos(angle+90), pos[1] + w*.5*math.sin(angle+90))
+        front_right = (pos[0] + w*.5*math.cos(angle-90), pos[1] + w*.5*math.sin(angle-90))
+        pos_back = (pos[0] - l*math.cos(angle), pos[1] - l*math.sin(angle))
+        back_left = (pos_back[0] + w*.5*math.cos(angle+90), pos_back[1] + w*.5*math.sin(angle+90))
+        back_right = (pos_back[0] + w*.5*math.cos(angle-90), pos_back[1] + w*.5*math.sin(angle-90))
+
+        cell_FL = self.point_to_cell(front_left)
+        cell_FR = self.point_to_cell(front_right)
+        cell_BL = self.point_to_cell(back_left)
+        cell_BR = self.point_to_cell(back_right)
+        cells = [cell_BL,cell_BR,cell_FR,cell_FL]
+        _24 = self.cells_per_side
+        xmin,xmax=min([cell[0] for cell in cells]), max([cell[0] for cell in cells])
+        ymin,ymax = min([cell[1] for cell in cells]), max([cell[1] for cell in cells])
+        # container_cells = [(xmin,ymin),(xmax,ymax)]
+
+        xmin_,xmax_ = max(0, min([cell[0] for cell in cells])), min(_24, max([cell[0] for cell in cells]))
+        ymin_,ymax_ = max(0, min([cell[1] for cell in cells])), min(_24 ,max([cell[1] for cell in cells]) )
+        container_cells = [(xmin_,xmax_),(ymin_,ymax_)]
+
+        v = self.currentspeed
+        # a = agent.car.accel #TODO:
+        a = 1
+        if v == self.V_range[-1] :
+            dc = max(math.ceil(v**2/2/a),math.ceil(v*self.dT))
+        elif v < self.V_range[-1]:
+            dc = max(math.ceil(v**2/2/a),math.ceil(v*self.dT+.5*a*self.dT))
+        else:
+            raise("Wrong value of V detected at cat",self.ID)
+        # dc_x = math.ceil(dc * math.cos(angle))  if dc * math.cos(angle) >0  else math.floor(dc * math.cos(angle))
+        # dc_y = math.ceil(dc * math.sin(angle))  if dc * math.sin(angle) >0  else math.floor(dc * math.sin(angle))
+        dc_x = round(dc * math.cos(angle))
+        dc_y = round(dc * math.sin(angle))
+        #print(dc,dc_x,dc_y)
+        xmin += dc_x
+        xmax += dc_x
+        ymin += dc_y
+        ymax += dc_y
+        xmin_,xmax_ =max(0, min([cell[0] for cell in cells])), min(_24, max([cell[0] for cell in cells]))
+        ymin_,ymax_ = max(0, min([cell[1] for cell in cells])), min(_24 ,max([cell[1] for cell in cells]))
+        # desired_cells = [(xmin,ymin),(xmax,ymax)]
+        desired_cells = [(xmin_, xmax_), (ymin_, ymax_)]
+        self.cont_cells = container_cells
+        self.desired_cells = desired_cells
+
+
+   ########################################## Actions Functions ##########################################
+    def acc(self):
+        if self.currentspeed < 4:
+            traci.vehicle.slowDown(self.ID,self.speedDict[self.currentspeed+1] , self.dT/10 )
+            self.currentspeed += 1
+            # traci.vehicle.setSpeed(self.ID,self.speedDict[self.currentspeed])
+            self.accl = 1
+
+    def dec(self):
+        if self.currentspeed > 0 :
+            traci.vehicle.slowDown(self.ID,self.speedDict[self.currentspeed-1] , self.dT/10 )
+            self.currentspeed -= 1
+            # traci.vehicle.setSpeed(self.ID,self.speedDict[self.currentspeed])
+            self.accl = -1
+
+
+    def keepgoing(self):
+        traci.vehicle.slowDown(self.ID, self.speedDict[self.currentspeed], self.dT / 10)
+        self.accl = 0
+
+
+
+
+    ############ end  #################
+
---
+++ version 2.0/runner.py
@@ -0,0 +1,142 @@
+#!/usr/bin/env python
+
+
+from __future__ import absolute_import
+from __future__ import print_function
+import pandas as pd
+import os
+import sys
+import optparse
+import random
+import pickle
+import numpy as np
+from collections import defaultdict
+# we need to import python modules from the $SUMO_HOME/tools directory
+if 'SUMO_HOME' in os.environ:
+    tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
+    sys.path.append(tools)
+else:
+    sys.exit("please declare environment variable 'SUMO_HOME'")
+
+from Autovechile import AutoVehicle
+from Autovechile import SingleAgent
+from Autovechile import env
+from sumolib import checkBinary  # noqa
+import traci  # noqa
+
+def generate_routefile(num_vehicles):
+    random.seed(420)  # make tests reproducible
+    dT = .5
+    N = 3600  # number of seconds
+    S = N/dT
+    with open("data/cross.rou.xml", "w") as routes:
+        print("""<routes>
+        <vType id="car" accel="3.5" decel="3.5" sigma="0.5" width="1.8" length="4.5" minGap="1" maxSpeed="25.2" guiShape="passenger"/>
+
+        <route id="right" edges="51o 1i 2o 52i" />
+        <route id="right_down" edges="51o 1i 3o 53i" />
+        <route id="right_up" edges="51o 1i 4o 54i" />
+        <route id="left" edges="52o 2i 1o 51i" />
+        <route id="left_down" edges="52o 2i 3o 53i" />
+        <route id="left_up" edges="52o 2i 4o 54i" />
+
+        <route id="up" edges="53o 3i 4o 54i" />
+,        <route id="up_right" edges="53o 3i 2o 52i" />
+        <route id="up_left" edges="53o 3i 1o 51i" />
+        <route id="down_right" edges="54o 4i 2o 52i" />
+        <route id="down_left" edges="54o 4i 1o 51i" />
+        <route id="down" edges="54o 4i 3o 53i" />
+        """, file=routes)
+        vehNr = 0
+        # demand per second from different directions
+        p = (num_vehicles/S)/12
+        # TODO: difference between time step and simulation step
+        myroutes = ['right','right_up','right_down','left','left_up','left_down',"up", "up_right" , "up_left" , "down_right" , "down_left" , "down"]
+        for i in np.arange(1, N , dT):
+            for route in myroutes:
+                r = random.uniform(0, 1)
+                if r < p:
+                    print(f'        <vehicle id="{route}_{vehNr}" type="car"  route="{route}" depart="{i}" />' , file=routes)
+                    vehNr += 1
+        print("</routes>", file=routes)
+
+def run(episode):
+    """execute the TraCI control loop"""
+    step = 0
+    #generate_routefile(3000)
+    existing_agents = []
+    my_env = env.env()
+    trainer = SingleAgent.SingleAgent(my_env)
+
+    while traci.simulation.getMinExpectedNumber() > 0:
+        step += 1
+        time = traci.simulation.getTime()
+        # print(time)
+
+        Q_i, Q_I = trainer.train(episode ,  existing_agents ,my_env)
+
+
+    with open("./output/Q_i.pickle", "wb") as f:
+        pickle.dump(Q_i, f)
+
+    with open("./output/Q_I_coordinated.pickle", "wb") as f2:
+        pickle.dump(Q_I, f2)
+
+    df = pd.DataFrame([ [state, actions_rewards] for state, actions_rewards in Q_i.items()],  columns=['state', 'Actions Rewards'])
+    print(df.to_csv("./output/qtable.csv"))
+
+    df = pd.DataFrame([[state, actions_rewards] for state, actions_rewards in Q_I.items()],
+                      columns=['state', 'Actions Rewards'])
+    print(df.to_csv("./output/qtableCoordinated.csv"))
+
+
+    traci.close()
+    sys.stdout.flush()
+
+
+def get_options():
+    optParser = optparse.OptionParser()
+    optParser.add_option("--nogui", action="store_true",
+                         default=False, help="run the commandline version of sumo")
+    optParser.add_option("--start", action="store",
+                          default=0, help="skip to specific time")
+    optParser.add_option("--step", action="store",help="set the step length to simulate at")
+    options, args = optParser.parse_args()
+    return options
+
+
+# this is the main entry point of this script
+if __name__ == "__main__":
+    options = get_options()
+
+    # this script has been called from the command line. It will start sumo as a
+    # server, then connect and run
+    if options.nogui:
+        sumoBinary = checkBinary('sumo')
+    else:
+        sumoBinary = checkBinary('sumo-gui')
+    print(sumoBinary)
+    # first, generate the route file for this simulation
+    #generate_routefile()
+
+    # this is the normal way of using traci. sumo is started as a
+    # subprocess and then the python script connects and runs
+    if options.step is None:
+        options.step = .1
+    #print((options.step))
+    # for-loop
+    # for i in range(100000000000,100000000001):
+    for i in range(1):
+        traci.start([sumoBinary, "-n", "2way-single-intersection/single-intersection.net.xml",
+                                 "-r" , "2way-single-intersection/single-intersection-vhvh.rou.xml",
+                                 "--tripinfo-output", "tripinfo.xml",
+                                 "--collision.mingap-factor", "0",
+                                 "--collision.action","none",
+                                 "--step-length", ".5",
+                                 "--time-to-teleport", "-1",
+                                 "--time-to-teleport.highways", "-1"]
+                                 # "--time-to-teleport.disconnected", "-1"]
+        )
+        # save the csv
+        print("Simulation Step: ", i)
+        run(i+1)
